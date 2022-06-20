0f15d8152cd9f0ed8bc8d2b7ea72af7ff581dd2b
hongsun2da <hongsun2da@gmail.com>
2022-06-20 17:22:57 +0900

    post 및 환경변수 선언

--- okrouter/src/pages/AjaxTest.jsx
+++ okrouter/src/pages/AjaxTest.jsx
@@ -2,33 +2,46 @@ import axios from "axios";
 import React from "react";
 
 function AjaxTest() {
-    const baseURL = "http://localhost:4000/api/hello";
+    const baseURL = import.meta.env.VITE_API_SERVER + "/api/hello";
     const [post, setPost] = React.useState(null);
+
+    async function getData() {
+      const data = {
+        name: document.getElementById('name').value
+      }
+      const response = await axios.post(baseURL, data);
+      console.log(response.data);
+      setPost(response.data);
+    }
+  
 
     React.useEffect(() => {
         // 동기 호출  
-        // axios.get(baseURL).then((response) => {
-        //     setPost(response.data);
-        // });
-
-        axios.post(baseURL, {
-            name: 'hongsun 입니다.'
-          }).then((response) => {
+        axios.get(baseURL).then((response) => {
             setPost(response.data);
         });
+
+        // axios.post(baseURL, {
+        //     name: 'hongsun 입니다.'
+        //   }).then((response) => {
+        //     setPost(response.data);
+        // });
        
 
-    // 비동기 호출 방법  
-    //   async function Doit(){
-    //     try{
-    //         const response = await axios.get(baseURL);
-    //         setPost(response.data);
-    //     } catch(error){
-    //         console.log(error);
-    //     }
-    //   }
+      // 비동기 호출 방법  
+      // async function SendName(){
+      //   try{
+      //       const data = {
+      //         name : document.getElementById('name').value
+      //       }
+      //       const response = await axios.post(baseURL, data)
+      //       setPost(response.data);
+      //   } catch(error){
+      //       console.log(error);
+      //   }
+      // }
 
-    //   Doit();
+      //SendName();
 
 
     }, []);
@@ -37,6 +50,8 @@ function AjaxTest() {
   
     return (
       <div>
+        <input type="name" id="name" />
+        <button onClick={getData}>send</button>
         <h1>{post.message}</h1>
       </div>
     );
