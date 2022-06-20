79af8d7986305da322488aa65238bd8c03f5abc2
hongsun2da <hongsun2da@gmail.com>
2022-06-20 15:29:20 +0900

    비동기 호출 추가

--- okrouter/src/pages/MyPage.jsx
+++ okrouter/src/pages/MyPage.jsx
@@ -8,15 +8,31 @@ function MyPage() {
     const [post, setPost] = React.useState(null);
 
     React.useEffect(() => {
-      axios.get(baseURL).then((response) => {
-        setPost(response.data);
-      });
+      // 동기 호출  
+      //   axios.get(baseURL).then((response) => {
+      //     setPost(response.data);
+      //   });
+
+      // 비동기 호출 방법  
+      async function Doit(){
+        try{
+            const response = await axios.get(baseURL);
+            setPost(response.data);
+        } catch(error){
+            console.log(error);
+        }
+      }
+
+      Doit();
+
+
     }, []);
   
     if (!post) return null;
   
     return (
-      <div>
+      <div id={post.id}>
+        {post.userId}
         <h1>{post.title}</h1>
         <p>{post.body}</p>
       </div>
